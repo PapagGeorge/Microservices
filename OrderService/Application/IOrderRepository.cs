56e1e6eb335def10c3554d1b11e60f89ae3e253e
ΓΙΩΡΓΟΣ ΠΑΠΑΓΕΩΡΓΙΟΥ <papageorgiou.90@gmail.com>
2024-12-08 21:40:52 +0200

    Set CRUD contructs

---
+++ OrderService/Application/IOrderRepository.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Models;
+
+namespace Application
+{
+    internal interface IOrderRepository
+    {
+        Task<Order> CreateOrderAsync(Order order);
+        // Read
+        Task<Order> GetOrderByIdAsync(Guid id);
+        Task<IEnumerable<Order>> GetOrdersAsync(int pageNumber, int pageSize);
+        Task<IEnumerable<Order>> GetOrdersByCustomerIdAsync(Guid customerId);
+        // Update
+        Task<Order> UpdateOrderAsync(Order order);
+    }
+}

18e56fdfe1620c9987cc9c024f6eb24a001e8f5c
ΓΙΩΡΓΟΣ ΠΑΠΑΓΕΩΡΓΙΟΥ <papageorgiou.90@gmail.com>
2024-12-24 15:24:52 +0200

    Set Models

---
+++ CustomerService/Domain/Models/Address.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json.Serialization;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Models;
+
+public class Address
+{
+    [Key]
+    [JsonPropertyName("id")]
+    public Guid Id { get; set; }
+
+    [JsonPropertyName("street")]
+    public string Street { get; set; }
+
+    [JsonPropertyName("city")]
+    public string City { get; set; }
+
+    [JsonPropertyName("state")]
+    public string State { get; set; }
+
+    [JsonPropertyName("postalCode")]
+    public string PostalCode { get; set; }
+
+    [JsonPropertyName("country")]
+    public string Country { get; set; }
+
+}
---
+++ CustomerService/Domain/Models/Customer.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.Json.Serialization;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+
+namespace Domain.Models;
+
+public class Customer
+{
+    [Key]
+    [JsonPropertyName("id")]
+    public Guid Id { get; set; }
+
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+
+    [JsonPropertyName("email")]
+    public string Email { get; set; }
+
+    [JsonPropertyName("mobilePhone")]
+    public string MobilePhone { get; set; }
+
+    [JsonPropertyName("address")]
+    public Address Address { get; set; }
+
+    [JsonPropertyName("status")]
+    public CustomerStatus Status { get; set; }
+
+    [JsonPropertyName("loyaltyPoints")]
+    public decimal LoyaltyPoints { get; set; }
+}
+
+public enum CustomerStatus
+{
+    Active,
+    Inactive
+}
